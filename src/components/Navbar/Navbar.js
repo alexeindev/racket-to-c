4acc36286889d7071dc5e53fd134e6f749148283
skystay12 <alejandralopez8212@gmail.com>
2020-04-29 08:27:33 -0500

    Navbar implementada

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from '../../assets/img/logo.png';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">
+          <img src={logo} class="d-inline-block align-top" alt="" width="30" />{' '}
+          Racket a C
+        </a>
+        <div class="navbar-nav-scroll">
+          <ul class="navbar-nav bd-navbar-nav flex-row">
+            <li class="nav-item">
+              <a class="nav-link " href="/">
+                Inicio
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+export default Navbar;
