beba31a0d5bab2089191ea657d5798643a5bf5e5
Nicolas Orozco <nicolas.orozco@utp.edu.co>
2020-04-29 20:02:57 -0500

    gome

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ class Card extends Component {
       <div className="card text-center overflow ">
         <img className="card-img-top" src={this.props.img} alt="Clases" />
         <div className="card-body text-dark">
-          <h4 className="card-title ">{this.props.title}</h4>
+          <h4 className="card-title title-align">{this.props.title}</h4>
           <p className="card-text text-secondary">{this.props.description}</p>
           <a href={this.props.page} className="btn btn-outline-primary">
             Iniciar tema
