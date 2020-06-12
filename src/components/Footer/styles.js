25f64cefbde8c48045e832ef65b0043a653bc679
Sebastían Garcia <36606545+SGarcia710@users.noreply.github.com>
2020-06-12 12:34:47 -0500

    Footer implementado

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-bottom: 30px;
+`;
+
+export const Text = styled.div`
+  margin-top: 30px;
+  padding-bottom: 20px;
+`;
+
+export const FooterImg = styled.img`
+  width: 40%;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 15%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+`;
--- src/pages/Practice/Practice.js
+++ src/pages/Practice/Practice.js
@@ -3,6 +3,8 @@ import './Practice.scss';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import Tabs from '../../components/Tabs/Tabs.jsx';
 import exercises from '../../assets/data/practicas.json';
+import Example from '../../components/TabCode/editores/EditorC';
+import Editor from '../../components/TabCode/editores/EditorRacket';
 
 function ejercicios(ejemplo, module) {
   var ejer;
@@ -27,21 +29,27 @@ function Practices(props) {
   const [currentActive, setCurrentActive] = useState(1);
   var name = dat.replace(':', '');
   const modul = name;
+
   return (
     <div className="Container">
       <Navbar mod={modul} />
       <div className="Practice">
         <h1 className="Practice__title">Herramienta de práctica</h1>
+        <Tabs
+          setCurrentActive={setCurrentActive}
+          currentActive={currentActive}
+          direction="row"
+          index={3}
+          type="Ejercicio"
+        />
         <div className="Practice__content">
           {/* Cambio en las tabs */}
-          <Tabs
-            setCurrentActive={setCurrentActive}
-            currentActive={currentActive}
-            direction="row"
-            index={3}
-            type="Ejercicio"
-          />
-          <div>{ejercicios(currentActive, modul)}</div>
+          <div className="Practice__text">
+            {ejercicios(currentActive, modul)}
+          </div>
+          <div className="Practice__editor">
+            <Editor></Editor>
+          </div>
         </div>
       </div>
     </div>
